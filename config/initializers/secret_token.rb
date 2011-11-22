ab2479b2874dce8db8ecbb85f9296af5db5b4da9
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-11-22 22:16:35 +0200

    Initial project import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sangwapo::Application.config.secret_token = '9f3294b830a9910c18a83a022d562f749259a5b783f1f0e3906ab6117451dda4a74fe6d9b0b31b29e71bf97a4e8ee4fa70e7738c38c5b9758a4f5cad505fe1e6'
