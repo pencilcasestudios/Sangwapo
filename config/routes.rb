7c8d1ca586746c997cb37dc3fe0add4f0cfdbf87
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-11-23 20:50:48 +0200

    Locale update. Layout tweak. Routes update.

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,13 @@ Sangwapo::Application.routes.draw do
 
 
   match "about", :to => "welcome#about", :as => "about"
+  match "account_settings", :to => "users#edit", :as => "account_settings"
+  match "contact", :to => "welcome#contact", :as => "contact"
+  match "privacy", :to => "welcome#privacy", :as => "privacy"
   match "sign_in", :to => "sessions#new", :as => "sign_in"
+  match "sign_out", :to => "sessions#destroy", :as => "sign_out"
   match "sign_up", :to => "users#new", :as => "sign_up"
   match "terms", :to => "welcome#terms", :as => "terms"
-  match "account_settings", :to => "users#edit", :as => "account_settings"
-  match "sign_out", :to => "sessions#destroy", :as => "sign_out"
 
 
 
