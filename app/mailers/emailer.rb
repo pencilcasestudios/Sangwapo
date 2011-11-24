317d880f3bad2394105cc09fb650094ca4a350a8
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-11-24 07:59:10 +0200

    Emailer update.

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -1,6 +1,6 @@
 class Emailer < ActionMailer::Base
   def registration_confirmation(user)
     @user = user
-    mail(to: user.email, subject: t("mailers.emailer.registration_confirmation.subject", application_name: t("application.name")), from: "#{t("application.name")} <#{AppConfig.email_user_name}>")
+    mail(to: "#{user.first_name} <#{user.email}>", subject: t("mailers.emailer.registration_confirmation.subject", application_name: t("application.name")), from: "#{t("application.name")} <#{AppConfig.email_user_name}>")
   end
 end
