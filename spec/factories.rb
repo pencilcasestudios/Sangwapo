2f5edb5f32eb38394ed11350a8dc9515b63fcdcb
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-11-24 12:22:55 +0200

    Factory update.

--- spec/factories.rb
+++ spec/factories.rb
@@ -13,6 +13,7 @@ Factory.define :listing do |f|
   
   f.sequence(:description) { |n| "Description #{n}" }
   f.sequence(:listing_code) { Listing.generate_listing_code }
+  f.sequence(:listing_type) { ListingType::NAMES[ListingType::NAMES.to_a[rand ListingType::NAMES.size].first] }  
   f.sequence(:panel_size) { PanelSize::NAMES[PanelSize::NAMES.to_a[rand PanelSize::NAMES.size].first] }  
   f.sequence(:uuid) { Listing.generate_uuid }
 
