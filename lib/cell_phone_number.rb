564a5b783c4d0e67fb1c842035938723aad447da
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-11-30 16:30:51 +0200

    Renamed PhoneNumber lib to CellPhoneNumber.

--- lib/cell_phone_number.rb
+++ lib/cell_phone_number.rb
@@ -1,4 +1,4 @@
-class PhoneNumber < ActiveRecord::Base
+class CellPhoneNumber < ActiveRecord::Base
   # Generate a random Zambian mobile carrier phone number sequence
   def self.random
     [["+26",""][rand 2],"09",["5","6","7"][rand 3],"#{'%07d' % (rand 10000000)}"].join.strip
