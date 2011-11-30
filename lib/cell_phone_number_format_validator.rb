b5c413394fd2c6c6a295ed57f57481c0699e7f09
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-11-30 16:31:30 +0200

    Renamed phone_number_format_validator.rb to cell_phone_number_format_validator.rb.

--- lib/cell_phone_number_format_validator.rb
+++ lib/cell_phone_number_format_validator.rb
@@ -1,5 +1,5 @@
 # Just capture the number as a person would enter it when make a call (optional leading '+')
-class PhoneNumberFormatValidator < ActiveModel::EachValidator
+class CellPhoneNumberFormatValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
     unless value =~ /^[\+]?[\d]{4,}$/
       object.errors[attribute] << (options[:message] || "is not formatted properly")
