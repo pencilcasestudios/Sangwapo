f532d4c65e79df6ceefa1f1b699b8e9cbc67c01c
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-12-02 15:04:35 +0200

    Using Listing::SIZES instead of PanelSize::NAMES.

--- lib/panel_size.rb
+++
@@ -1,8 +0,0 @@
-class PanelSize < ActiveRecord::Base
-  NAMES = {
-    # Translation                                               # Database key
-    I18n.t("models.panel_size.names.small")  =>                 "small",
-    I18n.t("models.panel_size.names.medium") =>                 "medium",
-    I18n.t("models.panel_size.names.large")  =>                 "large",
-  }
-end
