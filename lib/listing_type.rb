d804548770a0e481aa4a42c0faf23f92b2ab43e8
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-12-02 14:58:25 +0200

    Using Listing::TYPES instead of ListingTYPES::NAMES.

--- lib/listing_type.rb
+++
@@ -1,7 +0,0 @@
-class ListingType < ActiveRecord::Base
-  NAMES = {
-    # Translation                                     # Database key
-    I18n.t("models.listing_type.names.offered") =>    "offered",
-    I18n.t("models.listing_type.names.wanted")  =>    "wanted",
-  }
-end
