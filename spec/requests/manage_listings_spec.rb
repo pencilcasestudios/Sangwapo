b044e2c525f79d8c18fdb9e61efa22ae26619c13
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-12-02 15:26:42 +0200

    Updating views with the new translations with the newly factored Listing model.

--- spec/requests/manage_listings_spec.rb
+++ spec/requests/manage_listings_spec.rb
@@ -40,7 +40,7 @@ describe "Listing management" do
 
         select I18n.t("models.listing.types.offered"), from: I18n.t("views.listings._form.labels.listing_type")
 
-        select I18n.t("models.panel_size.names.small"), from: I18n.t("views.listings._form.labels.panel_size")
+        select I18n.t("models.listing.sizes.small"), from: I18n.t("views.listings._form.labels.panel_size")
 
         lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         fill_in I18n.t("views.listings._form.labels.description"), with: lorem
