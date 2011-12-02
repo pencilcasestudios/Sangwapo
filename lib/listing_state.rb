aeb3a88e944c89c2ea2f5d7349ac1ced645e3a53
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-12-02 14:53:23 +0200

    Using state_machine for Listing states.

--- lib/listing_state.rb
+++
@@ -1,8 +0,0 @@
-class ListingState < ActiveRecord::Base
-  NAMES = {
-    # Translation                                          # Database key
-    I18n.t("models.listing_state.names.unpublished") =>    "unpublished",
-    I18n.t("models.listing_state.names.approved")    =>    "approved",
-    I18n.t("models.listing_state.names.published")   =>    "published",
-  }
-end
