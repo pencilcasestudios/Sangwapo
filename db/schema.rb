183f6c4e35d688494d07f1d416bbe2b1017f3020
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-12-06 22:51:05 +0200

    delayed_job configuration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111202143537) do
+ActiveRecord::Schema.define(:version => 20111206203454) do
 
   create_table "comments", :force => true do |t|
     t.integer  "commentable_id"
@@ -24,6 +24,21 @@ ActiveRecord::Schema.define(:version => 20111202143537) do
   end
 
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "listings", :force => true do |t|
     t.boolean  "exclude_price",                                :default => false
