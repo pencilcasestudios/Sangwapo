cf9d39b8e7b106669708eb78a6139a7d9ac8296e
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-12-15 16:34:58 +0200

    Updated restricted cell phone number validation. It is now sensitive to the leading +260.

--- lib/unrestricted_cell_phone_number_validator.rb
+++ lib/unrestricted_cell_phone_number_validator.rb
@@ -1,7 +1,7 @@
 # Do not allow registration with numbers registered to this application
 class UnrestrictedCellPhoneNumberValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    if (CellPhoneNumber::RESTRICTED_NUMBERS.include?(value))
+    if (CellPhoneNumber::RESTRICTED_NUMBERS.include?(value.gsub(/^[\+]?(26)?/, ""))) # Strip the leading "+" or "+26"
       object.errors[attribute] << (options[:message] || I18n.t("validators.unrestricted_cell_phone_number.error"))
     end
   end
