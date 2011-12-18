00493654136c1eaf311fca8bf693e93c3884f965
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-12-18 20:51:22 +0200

    delayed_job table update.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111212181304) do
+ActiveRecord::Schema.define(:version => 20111218172626) do
 
   create_table "comments", :force => true do |t|
     t.integer  "commentable_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20111212181304) do
     t.datetime "locked_at"
     t.datetime "failed_at"
     t.string   "locked_by"
+    t.string   "queue"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
