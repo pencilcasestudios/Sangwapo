d6e16bd865492d3ad5399f7b855abf7b264e026f
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-12-27 18:19:04 +0200

    Corrected logic for restricted cell phone number validation.

--- lib/unrestricted_cell_phone_number_validator.rb
+++ lib/unrestricted_cell_phone_number_validator.rb
@@ -1,7 +1,7 @@
-# Do not allow registration with numbers registered to this application
+# Do not allow a User to create an account with a number to this application
 class UnrestrictedCellPhoneNumberValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    if (CellPhoneNumber::RESTRICTED_NUMBERS.include?(value.gsub(/^[\+]?(26)?/, ""))) # Strip the leading "+" or "+26"
+    if (value && CellPhoneNumber::RESTRICTED_NUMBERS.include?(value.gsub(/^[\+]?(26)?/, ""))) # Strip the leading "+" or "+26"
       object.errors[attribute] << (options[:message] || I18n.t("validators.unrestricted_cell_phone_number.error"))
     end
   end
