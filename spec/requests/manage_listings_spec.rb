878f819d021ebf5cbed8bbc2798b31be3af74e6d
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-12-27 19:17:22 +0200

    Respect the listing description character limit in the test spec.

--- spec/requests/manage_listings_spec.rb
+++ spec/requests/manage_listings_spec.rb
@@ -42,7 +42,7 @@ describe "Listing management" do
 
         select I18n.t("models.listing.sizes.small"), from: I18n.t("views.listings._form.labels.panel_size")
 
-        lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".truncate(140)
         fill_in I18n.t("views.listings._form.labels.description"), with: lorem
 
         select I18n.t("models.listing.periods")[7], from: I18n.t("views.listings._form.labels.display_for")
