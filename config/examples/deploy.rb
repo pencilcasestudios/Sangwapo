b29dea2d92e14846f95f885a705529f830e413cd
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2012-06-14 21:25:10 +0200

    Preparing for multi-stage deployment.

--- config/examples/deploy.rb
+++ config/examples/deploy.rb
@@ -3,24 +3,26 @@
 # http://beginrescueend.com/integration/capistrano/
 # http://thoughtsincomputation.com/posts/deploying-in-harmony-capistrano-rvm-bundler-and-git
 # http://stackoverflow.com/questions/6472785/bundler-error-on-deployment
-
+# https://github.com/capistrano/capistrano/wiki/2.x-Multistage-Extension
+# Ref: http://gist.github.com/293302
 
 
 
 # Basic steps to setup
 # ON THE SERVER
 
+# Create the MySQL database
+
 # Create the repository:
-# $ mkdir /var/Repositories/Git/#{application_name}.git
-# $ cd /var/Repositories/Git/#{application_name}.git
+# $ mkdir /var/Repositories/Git/#{application}.git
+# $ cd /var/Repositories/Git/#{application}.git
 # $ git init --bare
 
-# Create the remotes in the development repository
-# $ git remote add deployment #{user}@#{server_name}:/var/Repositories/Git/#{application_name}.git
+# Create the remotes to the production environment in the development repository
+# $ git remote add production #{user}@#{server_name}:/var/Repositories/Git/#{application}.git
 
 # Push to the repository on the server
-# $ git push deployment master
-# $ git push deployment deployment
+# $ git push production master
 
 # Create the RVM gemset
 # $ rvm gemset create <<whatever-gemset-you-require>>
@@ -31,41 +33,63 @@
 
 
 
+
 # ON THE DEVELOPMENT MACHINE
-# git push deployment deployment
+# STAGING DEPLOYMENT
+# $ git checkout master
+# $ git merge work
+# $ git push production
 # $ cap deploy:setup        # Update configuration files and create the database after this
 # $ cap deploy:check
 # $ cap deploy:update       # May require verifying the host key.
 # $ cap deploy:migrate
+# $ cap db:seed
 # $ cap deploy:start
 
+# PRODUCTION DEPLOYMENT
+# $ git checkout master
+# $ git merge work
+# $ git push production
+# $ cap production deploy:setup        # Update configuration files and create the database after this
+# $ cap production deploy:check
+# $ cap production deploy:update       # May require verifying the host key.
+# $ cap production deploy:migrate
+# $ cap production db:seed
+# $ cap production deploy:start
+# $ git tag "`date +published_%Y-%m-%d_%H%M%S`"
 
 
+
+
+# https://github.com/capistrano/capistrano/wiki/2.x-Multistage-Extension
+require "capistrano/ext/multistage"
 
 # https://github.com/collectiveidea/delayed_job/wiki/Rails-3-and-Capistrano
 require "delayed/recipes"  
+
+
+
+# Multi-stage deployment
+# https://github.com/capistrano/capistrano/wiki/2.x-Multistage-Extension
+#set :deploy_env, "production"
+set :stages, [DEPLOYMENT_CONFIG["stage_name_staging"], DEPLOYMENT_CONFIG["stage_name_production"]]
+set :default_stage, DEPLOYMENT_CONFIG["stage_name_staging"]
+
 set :rails_env, "production" # Added for delayed job  
 
-# Customise these application-specific settings by updating config/config.yml
+# Customise these application-specific settings by updating config.yml used by Settingslogic
 # DEPLOYMENT_CONFIG is initialised in Capfile
-set :application_name, DEPLOYMENT_CONFIG["application_name"]
+set :application, DEPLOYMENT_CONFIG["application_name"]
 set :gemset_name, DEPLOYMENT_CONFIG["gemset_name"]
 set :ruby_version, DEPLOYMENT_CONFIG["ruby_version"]
-set :server_name, DEPLOYMENT_CONFIG["server_name"]
+# set :server_name, DEPLOYMENT_CONFIG["server_name"] - Server name now gets configured in config/deploy/{stage_name}.rb
 set :user, DEPLOYMENT_CONFIG["user"]
-
-set :rvm_ruby_gemset, "#{ruby_version}@#{gemset_name}"
-
-
-
-
 set :bundle_without, [:darwin, :development, :test]
-
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))              # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                                            # Load RVM's capistrano plugin.
-# Don't forget to create gemset on the server
+set :rvm_ruby_gemset, "#{ruby_version}@#{gemset_name}"              # Don't forget to create gemset on the server
 set :rvm_ruby_string, "#{rvm_ruby_gemset}"                          # Select the gemset
-set :rvm_type, :user                                                # RVM install is in the deploying user's home directory
+
+# Load RVM's capistrano plugin.
+require "rvm/capistrano"                                            
 
 
 
@@ -77,15 +101,15 @@ depend :remote, :gem, "rake", ">=0.9.2.2"
 
 
 
-set :application, DEPLOYMENT_CONFIG["server_name"]
-role :app, application
-role :web, application
-role :db,  application, :primary => true
+#set :server_name, DEPLOYMENT_CONFIG["server_name"] - now gets configured in config/deploy/{stage_name}.rb
+#role :app, application
+#role :web, application
+#role :db,  application, :primary => true
 
 
 
 
-set :deploy_to, "/var/Apps/#{application_name}"
+# set :deploy_to, "/var/Apps/#{application}" - now gets configured in config/deploy/{stage_name}.rb
 set :deploy_via, :remote_cache
 set :copy_cache, true
 set :use_sudo, false
@@ -95,9 +119,10 @@ set :use_sudo, false
 
 set :scm, :git
 # Don't forget to make this repo on the server
-set :repository, "#{user}@#{server_name}:/var/Repositories/Git/#{application_name}.git"
+set :repository_server_name, DEPLOYMENT_CONFIG["repository_server_name"]
+set :repository, "#{user}@#{repository_server_name}:/var/Repositories/Git/#{application}.git"
 # Don't forget to make this branch in the repository
-set :branch, DEPLOYMENT_CONFIG["deployment_branch"]
+set :branch, DEPLOYMENT_CONFIG["repository_deployment_branch"]
 
 
 
@@ -114,20 +139,52 @@ namespace :deploy do
   desc "Setup shared directory."
   task :setup_shared do
     run "mkdir #{shared_path}/config"
-    put File.read("config/examples/database.yml"), "#{shared_path}/config/database.yml"
     put File.read("config/examples/config.yml"), "#{shared_path}/config/config.yml"
+    put File.read("config/examples/database.yml"), "#{shared_path}/config/database.yml"
+    #put File.read("config/examples/twitter.yml"), "#{shared_path}/config/twitter.yml"
     puts "Now edit the config files in #{shared_path}."
   end
 
   desc "Symlink extra configs and folders."
   task :symlink_extras do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/config.yml #{release_path}/config/config.yml"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    #run "ln -nfs #{shared_path}/config/twitter.yml #{release_path}/config/twitter.yml"
   end
 end
 
 
 
+
+# Database tasks
+# Ref: https://gist.github.com/157958
+namespace :db do
+  desc "Populates the Production Database"
+  task :seed do
+    puts "\n\n=== Populating the Production Database! ===\n\n"
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=production"
+  end
+end
+
+
+
+
+# Server tasks
+# Ref: https://github.com/capistrano/capistrano/wiki/2.x-Multistage-Extension
+namespace :deployment_server do
+  desc "Display the (UNIX) server operating system name"
+  task :uname do
+    run "uname -a"
+  end
+end
+
+
+
+
+# http://beginrescueend.com/integration/capistrano/
+# RVM-Capistrano
+before "deploy:setup", "rvm:install_rvm"
+before "deploy", "rvm:install_rvm"
 
 after "deploy", "deploy:cleanup" # keeps only last 5 releases
 after "deploy:setup", "deploy:setup_shared"
