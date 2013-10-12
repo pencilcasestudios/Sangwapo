aec081661a2fb929f4e46c272a800391b0c7ddcc
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-10-12 16:09:01 +0200

    Allow start TLS to be configured in the config.

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   address:              AppConfig.email_address,
   authentication:       "plain",
   domain:               AppConfig.email_domain,
-  enable_starttls_auto: true,
+  enable_starttls_auto: AppConfig.email_enable_starttls_auto,
   password:             AppConfig.email_password,
   port:                 AppConfig.email_port,
   user_name:            AppConfig.email_user_name,
