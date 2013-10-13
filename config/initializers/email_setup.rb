ffb8299555ea5b29464efbd8d6c9cdd5c4176f5b
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-10-13 16:38:35 +0200

    Allow more email configuration settings to be defined in config.yml.

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -1,11 +1,12 @@
 ActionMailer::Base.smtp_settings = {
-  address:              AppConfig.email_address,
-  authentication:       "plain",
-  domain:               AppConfig.email_domain,
-  enable_starttls_auto: AppConfig.email_enable_starttls_auto,
-  password:             AppConfig.email_password,
-  port:                 AppConfig.email_port,
-  user_name:            AppConfig.email_user_name,
+	address:              AppConfig.email_address,
+	authentication:       AppConfig.email_authentication,
+	domain:               AppConfig.email_domain,
+	enable_starttls_auto: AppConfig.email_enable_starttls_auto,
+	openssl_verify_mode:	AppConfig.email_openssl_verify_mode,
+	password:             AppConfig.email_password,
+	port:                 AppConfig.email_port,
+	user_name:            AppConfig.email_user_name,
 }
 
 ActionMailer::Base.default_url_options[:host] = AppConfig.application_server_name
