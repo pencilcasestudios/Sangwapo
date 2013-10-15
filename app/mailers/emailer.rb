900fa3622c3fefdceec157b88ffe6cc60e311e1c
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-10-15 10:31:41 +0200

    Refactored emailer.rb.

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -1,26 +1,28 @@
 class Emailer < ActionMailer::Base
+  default from: "#{I18n.t("application.name")} <#{AppConfig.email_user_name}>"
+
   def registration_confirmation(user)
     @user = user
-    mail(to: "#{user.first_name} <#{user.email}>", subject: t("mailers.emailer.registration_confirmation.subject", application_name: t("application.name")), from: "#{t("application.name")} <#{AppConfig.email_user_name}>")
+    mail(to: "#{user.first_name} <#{user.email}>", subject: t("mailers.emailer.registration_confirmation.subject", application_name: t("application.name")))
   end
 
   def payment_received_confirmation(payment)
     @payment = payment
-    mail(to: "#{payment.user.first_name} <#{payment.user.email}>", subject: t("mailers.emailer.payment_received_confirmation.subject", application_name: t("application.name")), from: "#{t("application.name")} <#{AppConfig.email_user_name}>")
+    mail(to: "#{payment.user.first_name} <#{payment.user.email}>", subject: t("mailers.emailer.payment_received_confirmation.subject", application_name: t("application.name")))
   end
 
   def listing_approved_confirmation(listing)
     @listing = listing
-    mail(to: "#{listing.user.first_name} <#{listing.user.email}>", subject: t("mailers.emailer.listing_approved_confirmation.subject", application_name: t("application.name")), from: "#{t("application.name")} <#{AppConfig.email_user_name}>")
+    mail(to: "#{listing.user.first_name} <#{listing.user.email}>", subject: t("mailers.emailer.listing_approved_confirmation.subject", application_name: t("application.name")))
   end
 
   def listing_rejected_confirmation(listing)
     @listing = listing
-    mail(to: "#{listing.user.first_name} <#{listing.user.email}>", subject: t("mailers.emailer.listing_rejected_confirmation.subject", application_name: t("application.name")), from: "#{t("application.name")} <#{AppConfig.email_user_name}>")
+    mail(to: "#{listing.user.first_name} <#{listing.user.email}>", subject: t("mailers.emailer.listing_rejected_confirmation.subject", application_name: t("application.name")))
   end
 
   def expired_listing_notification(listing)
     @listing = listing
-    mail(to: "#{listing.user.first_name} <#{listing.user.email}>", subject: t("mailers.emailer.expired_listing_notification.subject", application_name: t("application.name")), from: "#{t("application.name")} <#{AppConfig.email_user_name}>")
+    mail(to: "#{listing.user.first_name} <#{listing.user.email}>", subject: t("mailers.emailer.expired_listing_notification.subject", application_name: t("application.name")))
   end
 end
