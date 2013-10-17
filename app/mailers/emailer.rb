06d84aed34c9cf530a310c4f6adefd546ca618a9
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-10-17 08:22:12 +0200

    Email logging no longer necessary.

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -7,9 +7,6 @@ class Emailer < ActionMailer::Base
       subject: t("mailers.emailer.registration_confirmation.subject", application_name: t("application.name")),
       to: "#{user.first_name} <#{user.email}>",
     )
-
-    Delayed::Worker.logger.debug "\nUser attributes #{user.inspect}"
-    Delayed::Worker.logger.debug "\nMail attributes #{mail.to_s}"
   end
 
   def payment_received_confirmation(payment)
