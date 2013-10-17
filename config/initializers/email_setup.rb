db7465cb44856cc5c29231aab090ab641a162c6a
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-10-17 08:13:15 +0200

    Quick-and-dirty Postfix setup for the virtual machine figured out. Updated example configs to make use of this.

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -1,12 +1,18 @@
-ActionMailer::Base.smtp_settings = {
-	address:              AppConfig.email_address,
-	authentication:       AppConfig.email_authentication,
-	domain:               AppConfig.email_domain,
-	enable_starttls_auto: AppConfig.email_enable_starttls_auto,
-	openssl_verify_mode:	AppConfig.email_openssl_verify_mode,
-	password:             AppConfig.email_password,
-	port:                 AppConfig.email_port,
-	user_name:            AppConfig.email_user_name,
-}
+if AppConfig.email_delivery_mode == "virtual" # Deliver using postfix on the virtual machine
+	ActionMailer::Base.smtp_settings = {
+		openssl_verify_mode:		AppConfig.email_openssl_verify_mode,
+	}
+else
+	ActionMailer::Base.smtp_settings = {
+		address:              	AppConfig.email_address,
+		authentication:       	AppConfig.email_authentication,
+		domain:               	AppConfig.email_domain,
+		enable_starttls_auto: 	AppConfig.email_enable_starttls_auto,
+		openssl_verify_mode:		AppConfig.email_openssl_verify_mode,
+		password:             	AppConfig.email_password,
+		port:                 	AppConfig.email_port,
+		user_name:            	AppConfig.email_user_name,
+	}
+end
 
 ActionMailer::Base.default_url_options[:host] = AppConfig.application_server_name
