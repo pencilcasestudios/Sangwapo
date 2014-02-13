e0ac10cbc2a48b9b6f6ee714fc60270e83303ed8
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2014-02-13 09:25:39 +0200

    Updated example deployment configuration.

--- config/examples/deploy/production.rb
+++ config/examples/deploy/production.rb
@@ -1,10 +1,10 @@
 # Ref: http://gist.github.com/293302
-server DEPLOYMENT_CONFIG["server_name_production"], :web, :app, :db, primary: true
-set :deployment_path, DEPLOYMENT_CONFIG["deployment_path_production"]
+server DEPLOYMENT_CONFIG["production_server_name"], :web, :app, :db, primary: true
+set :deployment_path, DEPLOYMENT_CONFIG["production_deployment_path"]
 set :deploy_to, "/var/Apps/#{application}/#{deployment_path}"
 
 # Don't forget to make this repo on the server
-set :repository_server_name, DEPLOYMENT_CONFIG["repository_server_name"]
+set :repository_server_name, DEPLOYMENT_CONFIG["production_server_name"]
 set :repository, "#{user}@#{repository_server_name}:/var/Repositories/Git/#{application}.git"
 # Don't forget to make this branch in the repository
-set :branch, DEPLOYMENT_CONFIG["repository_deployment_branch"]
+set :branch, DEPLOYMENT_CONFIG["production_repository_deployment_branch"]
--- config/examples/deploy/staging.rb
+++ config/examples/deploy/staging.rb
@@ -1,10 +1,10 @@
 # Ref: http://gist.github.com/293302
-server DEPLOYMENT_CONFIG["server_name_staging"], :web, :app, :db, primary: true
-set :deployment_path, DEPLOYMENT_CONFIG["deployment_path_staging"]
+server DEPLOYMENT_CONFIG["staging_server_name"], :web, :app, :db, primary: true
+set :deployment_path, DEPLOYMENT_CONFIG["staging_deployment_path"]
 set :deploy_to, "/var/Apps/#{application}/#{deployment_path}"
 
 # Don't forget to make this repo on the server
-set :repository_server_name, DEPLOYMENT_CONFIG["repository_server_name"]
+set :repository_server_name, DEPLOYMENT_CONFIG["staging_server_name"]
 set :repository, "#{user}@#{repository_server_name}:/var/Repositories/Git/#{application}.git"
 # Don't forget to make this branch in the repository
-set :branch, DEPLOYMENT_CONFIG["repository_deployment_branch"]
+set :branch, DEPLOYMENT_CONFIG["staging_repository_deployment_branch"]
