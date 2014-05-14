046c56c0a5fbb23724ba882a6c1d2e51281ec4bf
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2014-05-14 09:50:46 +0200

    Ruby 2 syntax.

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -1,10 +1,10 @@
 def sign_in_with_email
-  @current_user = FactoryGirl.create(:user)  
+  @current_user = FactoryGirl.create(:user)
 
   visit sign_in_path
 
-  fill_in I18n.t("views.sessions._form.labels.identifier"), :with => @current_user.email
-  fill_in I18n.t("views.sessions._form.labels.password"), :with => AppConfig.test_user_password
+  fill_in I18n.t("views.sessions._form.labels.identifier"), with: @current_user.email
+  fill_in I18n.t("views.sessions._form.labels.password"), with: AppConfig.test_user_password
 
   click_button I18n.t("views.sessions._form.buttons.submit")
   page.should have_content(I18n.t("views.application._navigation.links.sign_out"))
@@ -12,12 +12,12 @@ def sign_in_with_email
 end
 
 def sign_in_with_cell_phone_number
-  @current_user = FactoryGirl.create(:user)  
+  @current_user = FactoryGirl.create(:user)
 
   visit sign_in_path
 
-  fill_in I18n.t("views.sessions._form.labels.identifier"), :with => @current_user.cell_phone_number
-  fill_in I18n.t("views.sessions._form.labels.password"), :with => AppConfig.test_user_password
+  fill_in I18n.t("views.sessions._form.labels.identifier"), with: @current_user.cell_phone_number
+  fill_in I18n.t("views.sessions._form.labels.password"), with: AppConfig.test_user_password
 
   click_button I18n.t("views.sessions._form.buttons.submit")
   page.should have_content(I18n.t("views.application._navigation.links.sign_out"))
